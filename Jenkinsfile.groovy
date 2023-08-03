e22ca6ccaf9f34adc8d73b9632b2d50e7a5c0742
deepucsk847 <67491118+deepucsk847@users.noreply.github.com>
2023-08-03 19:30:11 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,16 +2,15 @@ pipeline {
     agent any
 
     environment {
-        DOCKER_REGISTRY = "localhost:5000"
-        DOCKER_IMAGE_NAME = "nginx-kubernetes"
-        DOCKER_IMAGE_TAG = "latest"
-        K8S_NAMESPACE = "nginx-namespace"
-        K8S_DEPLOYMENT_NAME = "nginx-deployment"
-        JENKINSFILE_ENV_PATH = "Jenkinsfile.env" // Replace with the correct path
+        DOCKER_REGISTRY = 'localhost:5000'
+        DOCKER_IMAGE_NAME = 'nginx-kubernetes'
+        DOCKER_IMAGE_TAG = 'latest'
+        K8S_NAMESPACE = 'nginx-namespace'
+        K8S_DEPLOYMENT_NAME = 'nginx-deployment'
     }
 
     stages {
-        stage('Checkout SCM') {
+        stage('Declarative: Checkout SCM') {
             steps {
                 checkout scm
             }
@@ -20,49 +19,38 @@ pipeline {
         stage('Read Jenkinsfile.env') {
             steps {
                 script {
-                    def jenkinsfileEnv = readFile(env.JENKINSFILE_ENV_PATH).trim()
-                    def envMap = new groovy.json.JsonSlurper().parseText(jenkinsfileEnv)
-                    DOCKER_REGISTRY = envMap.DOCKER_REGISTRY ?: DOCKER_REGISTRY
-                    DOCKER_IMAGE_NAME = envMap.DOCKER_IMAGE_NAME ?: DOCKER_IMAGE_NAME
-                    DOCKER_IMAGE_TAG = envMap.DOCKER_IMAGE_TAG ?: DOCKER_IMAGE_TAG
-                    K8S_NAMESPACE = envMap.K8S_NAMESPACE ?: K8S_NAMESPACE
-                    K8S_DEPLOYMENT_NAME = envMap.K8S_DEPLOYMENT_NAME ?: K8S_DEPLOYMENT_NAME
-                }
-            }
-        }
+                    def jenkinsfileEnvPath = "Jenkinsfile.env" // Replace with the correct path
+                    def jenkinsfileEnvContent = readFile(jenkinsfileEnvPath).trim()
 
-        stage('Build Docker Image') {
-            when {
-                expression { return env.BRANCH_NAME == 'master' }
-            }
-            steps {
-                script {
-                    docker.build("${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}")
-                    docker.withRegistry("${DOCKER_REGISTRY}", 'docker-hub-credentials') {
-                        dockerImage.push("${DOCKER_IMAGE_TAG}")
+                    echo "Jenkinsfile.env contents:"
+                    echo jenkinsfileEnvContent
+
+                    try {
+                        def envMap = new groovy.json.JsonSlurper().parseText(jenkinsfileEnvContent)
+                        DOCKER_REGISTRY = envMap.DOCKER_REGISTRY ?: DOCKER_REGISTRY
+                        DOCKER_IMAGE_NAME = envMap.DOCKER_IMAGE_NAME ?: DOCKER_IMAGE_NAME
+                        DOCKER_IMAGE_TAG = envMap.DOCKER_IMAGE_TAG ?: DOCKER_IMAGE_TAG
+                        K8S_NAMESPACE = envMap.K8S_NAMESPACE ?: K8S_NAMESPACE
+                        K8S_DEPLOYMENT_NAME = envMap.K8S_DEPLOYMENT_NAME ?: K8S_DEPLOYMENT_NAME
+                    } catch (Exception e) {
+                        echo "Error parsing Jenkinsfile.env as JSON: ${e.message}"
                     }
                 }
             }
         }
 
-        stage('Deploy to Kubernetes') {
-            when {
-                expression { return env.BRANCH_NAME == 'master' }
-            }
-            steps {
-                // Your Kubernetes deployment steps here
-                // For example, using kubectl to apply manifests, etc.
-                sh "kubectl apply -n ${K8S_NAMESPACE} -f your-kubernetes-manifest.yaml"
-            }
+        stage('Build Docker Image') {
+            // Your build steps here
         }
-    }
 
-    post {
-        success {
-            echo 'Deployment successful!'
+        stage('Deploy to Kubernetes') {
+            // Your deployment steps here
         }
-        failure {
-            echo 'Deployment failed!'
+
+        stage('Declarative: Post Actions') {
+            steps {
+                echo 'Deployment failed!'
+            }
         }
     }
 }
