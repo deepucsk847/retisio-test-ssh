3ba60d5b0993a605e54d27fbe1356cc1eb43a5c5
deepucsk847 <67491118+deepucsk847@users.noreply.github.com>
2023-08-03 19:40:07 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,60 +1,42 @@
-pipeline {
-    agent any
-
-    environment {
-        DOCKER_REGISTRY = 'localhost:5000'
-        DOCKER_IMAGE_NAME = 'nginx-kubernetes'
-        DOCKER_IMAGE_TAG = 'latest'
-        K8S_NAMESPACE = 'nginx-namespace'
-        K8S_DEPLOYMENT_NAME = 'nginx-deployment'
+node {
+    stage('Declarative: Checkout SCM') {
+        checkout scm
     }
 
-    stages {
-        stage('Declarative: Checkout SCM') {
-            steps {
-                checkout scm
-            }
+    stage('Read Jenkinsfile.env') {
+        def jenkinsfileEnvPath = "path/to/Jenkinsfile.env" // Replace with the correct path
+        def jenkinsfileEnvContent = readFile(jenkinsfileEnvPath).trim()
+
+        echo "Jenkinsfile.env contents:"
+        echo jenkinsfileEnvContent
+
+        try {
+            def envMap = new groovy.json.JsonSlurper().parseText(jenkinsfileEnvContent)
+            env.DOCKER_REGISTRY = envMap.DOCKER_REGISTRY ?: 'localhost:5000'
+            env.DOCKER_IMAGE_NAME = envMap.DOCKER_IMAGE_NAME ?: 'nginx-kubernetes'
+            env.DOCKER_IMAGE_TAG = envMap.DOCKER_IMAGE_TAG ?: 'latest'
+            env.K8S_NAMESPACE = envMap.K8S_NAMESPACE ?: 'nginx-namespace'
+            env.K8S_DEPLOYMENT_NAME = envMap.K8S_DEPLOYMENT_NAME ?: 'nginx-deployment'
+        } catch (Exception e) {
+            echo "Error parsing Jenkinsfile.env as JSON: ${e.message}"
         }
+    }
 
-        stage('Read Jenkinsfile.env') {
-            steps {
-                script {
-                    def jenkinsfileEnvPath = "Jenkinsfile.env" // Replace with the correct path 
-                    def jenkinsfileEnvContent = readFile(jenkinsfileEnvPath).trim()
+    stage('Build Docker Image') {
+        // Your build steps here
+        echo "Building Docker Image..."
+        // For example:
+        sh 'docker build -t ${env.DOCKER_REGISTRY}/${env.DOCKER_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG} .'
+    }
 
-                    echo "Jenkinsfile.env contents:"
-                    echo jenkinsfileEnvContent
+    stage('Deploy to Kubernetes') {
+        // Your deployment steps here
+        echo "Deploying to Kubernetes..."
+        // For example:
+        sh "kubectl apply -f kubernetes/deployment.yaml --namespace=${env.K8S_NAMESPACE} --record"
+    }
 
-                    try {
-                        def envMap = new groovy.json.JsonSlurper().parseText(jenkinsfileEnvContent)
-                        DOCKER_REGISTRY = envMap.DOCKER_REGISTRY ?: DOCKER_REGISTRY
-                        DOCKER_IMAGE_NAME = envMap.DOCKER_IMAGE_NAME ?: DOCKER_IMAGE_NAME
-                        DOCKER_IMAGE_TAG = envMap.DOCKER_IMAGE_TAG ?: DOCKER_IMAGE_TAG
-                        K8S_NAMESPACE = envMap.K8S_NAMESPACE ?: K8S_NAMESPACE
-                        K8S_DEPLOYMENT_NAME = envMap.K8S_DEPLOYMENT_NAME ?: K8S_DEPLOYMENT_NAME
-                    } catch (Exception e) {
-                        echo "Error parsing Jenkinsfile.env as JSON: ${e.message}"
-                    }
-                }
-            }
-        }
-
-        stage('Build Docker Image') {
-            steps {
-                // Your build steps here
-            }
-        }
-
-        stage('Deploy to Kubernetes') {
-            steps {
-                // Your deployment steps here
-            }
-        }
-
-        stage('Declarative: Post Actions') {
-            steps {
-                echo 'Deployment failed!'
-            }
-        }
+    stage('Declarative: Post Actions') {
+        echo 'Deployment failed!'
     }
 }
